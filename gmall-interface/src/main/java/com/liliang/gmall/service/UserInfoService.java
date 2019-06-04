a07b614a42e8b66fd2de8c088c6a924c05c42684
liliang <1254842504@qq.com>
2019-06-04 19:41:54 +0800

    初步搭建在线电商项目，采用spring boot +dubbo.

---
+++ gmall-interface/src/main/java/com/liliang/gmall/service/UserInfoService.java
@@ -0,0 +1,25 @@
+package com.liliang.gmall.service;
+
+import com.liliang.gmall.bean.UserAddress;
+import com.liliang.gmall.bean.UserInfo;
+
+import java.util.List;
+
+public interface UserInfoService {
+
+    /**
+     * 查询所有用户信息
+     *
+     * @return
+     */
+    List<UserInfo> findAll();
+
+    /**
+     * 根据用户Id 查询用户地址列表
+     *
+     * @param userId
+     * @return
+     */
+    UserAddress findAddressByUserId(String userId);
+
+}
