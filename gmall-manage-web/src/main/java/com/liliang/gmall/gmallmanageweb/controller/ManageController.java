a68ceebb7e2519c34cbda2a8edfc4d3272f60f1c
liliang <1254842504@qq.com>
2019-06-06 09:41:12 +0800

    添加商品分类的manage_service和manage_web两个微服务，manage_service是负责业务逻辑的处理，manage_web负责前端控制。本次主要添加的方法的功能为，查询所有的一级分类，根据传入的一级分类id得到二级分类，根据二级分类的id查询得到三级分类，根据三级分类的id，查询到所有的属性值，添加/修改，属性和属性值。

---
+++ gmall-manage-web/src/main/java/com/liliang/gmall/gmallmanageweb/controller/ManageController.java
@@ -0,0 +1,85 @@
+package com.liliang.gmall.gmallmanageweb.controller;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.liliang.gmall.bean.*;
+import com.liliang.gmall.service.ManageService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@CrossOrigin
+public class ManageController {
+
+    @RequestMapping("hello")
+    public String hello(){
+
+        return "hello";
+    }
+
+//    getCatalog1
+    @Reference
+    private ManageService manageService;
+
+    /**
+     * 获取到所有的一级分类
+     * @return
+     */
+    @RequestMapping("getCatalog1")
+    @ResponseBody
+    public List<BaseCatalog1> getCatalog1(){
+        List<BaseCatalog1> list = manageService.getCatalog1();
+        return list;
+    }
+    //getCatalog2
+
+    /**
+     * 根据一级分类的id，获取到该一级分类的所有的二级分类
+     * @param catalog1Id
+     * @return
+     */
+    @RequestMapping("getCatalog2")
+    @ResponseBody
+    public List<BaseCatalog2> getCatalog2(@RequestParam String catalog1Id){
+        List<BaseCatalog2> list = manageService.getCatalog2(catalog1Id);
+        return list;
+    }
+
+    /**
+     * 根据二级分类的id，获取到该二级分类的所有的三级分类
+     * @param catalog2Id
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping("getCatalog3")
+    public List<BaseCatalog3> getCatalog3(@RequestParam String catalog2Id){
+        List<BaseCatalog3> list = manageService.getCatalog3(catalog2Id);
+        return list;
+    }
+
+    //attrInfoList
+    @RequestMapping("attrInfoList")
+    @ResponseBody
+    public List<BaseAttrInfo> attrInfoList(@RequestParam String catalog3Id){
+        List<BaseAttrInfo> attrList = manageService.getAttrList(catalog3Id);
+        return attrList;
+    }
+
+    //saveAttrInfo
+    @ResponseBody
+    @RequestMapping("saveAttrInfo")
+    public String saveAttrInfo(@RequestBody BaseAttrInfo baseAttrInfo){
+        String value = manageService.saveAttrInfo(baseAttrInfo);
+        return value;
+    }
+
+    //getAttrValueList?attrId=
+    @RequestMapping("getAttrValueList")
+    @ResponseBody
+    public List<BaseAttrValue> getAttrValueListByAttrId(@RequestParam String attrId){
+        List<BaseAttrValue> baseAttrValues = manageService.getAttrValueListByAttrId(attrId);
+        return baseAttrValues;
+    }
+}
+
